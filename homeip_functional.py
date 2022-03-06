d976eb4d2f42dbb3c0bfa7b07fa23c251415b1c2
wmhernandez <wmhernandez@hotmail.com>
2022-03-05 22:23:18 -0500

    Add files via upload
    This was a program I created to check my home's ip address.  I was being bumped off the network regularly by my service provider, and I wanted to see if my home ip was changing in the process.
    Also, I was logging on remotely to my home network via putty, and I wanted to have an up to date ip address available.  The program would send me an email regularly using an hotmail account.  In a later version the ip was recorded by date to an another file, for later analysis.

---
+++ homeip_functional.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# home ip address checker.  Regular, 1/wk checker, for outside accessing.
+
+# send an info request to the my address website.
+# How do I access the google info, directly?
+
+import socket
+import sys
+import email
+import smtplib
+import urllib.request, urllib.parse, urllib.error  #  this from 12.5 Bsoup section
+# from bs4 import BeautifulSoup
+import ssl
+import datetime
+#---------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------
+#Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+#---------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------
+
+
+# *****  basic email send *******
+
+msg = email.message_from_string(theip.decode())
+# print(type(theip.decode()))
+
+msg['From'] = " XXXXXXXXXX"
+msg['To'] = "XXXXXXXXXXXX"
+msg['Subject'] = "IP program test successful"
+
+s = smtplib.SMTP("smtp.live.com",587) #email server connection.  This is set for hotmail.com
+s.ehlo() # Hostname to send for this command defaults to the fully qualified domain name of the local host.
+s.starttls() #Puts connection to SMTP server in TLS mode
+s.ehlo()
+s.login('XXXXXXXX', 'XXXXXXXX')
+
+s.sendmail("XXXXXXXXXXXXXX", "XXXXXXXXXXXXX", msg.as_string())
+
+s.quit()
+
+ #----------------------------------------------------------------------------------------
